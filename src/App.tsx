488838b03d714c5c5cc72199c264b3299738a15d
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-13 16:51:48 +0300

    Add Container component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Heading from "./components/Heading";
 import Oscar from "./components/Oscar";
 import Button from "./components/Button";
 import Input from "./components/Input";
+import Container from "./components/Container";
 
 function App() {
   const person = {
@@ -45,6 +46,7 @@ function App() {
       <Button
         handleClick={(event, id) => console.log("Button clicked", event, id)}
       />
+      <Container styles={{ border: "1px solid black", padding: "1rem" }} />
       <Input
         value=""
         handleChange={(event) => console.log(event.target.value)}
